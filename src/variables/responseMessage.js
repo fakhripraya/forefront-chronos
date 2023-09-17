8d6aa1d40d930c2bc1f22518882f680bcfe11d4b
fakhriprayatna.putra <fakhriprayatna.putra@homecredit.co.id>
2023-09-17 14:13:54 +0700

    Update responseMessage.js

--- src/variables/responseMessage.js
+++ src/variables/responseMessage.js
@@ -1,24 +1,29 @@
 // GENERALS ERRORS
-const UNIDENTIFIED_ERROR = "Ada yang salah ni kawan, coba kontak customer service ya biar kamu dibantuin !";
-const STORE_ALREADY_EXIST = "Toko sudah ada, coba pakai nama lain untuk toko baru anda !";
+const UNIDENTIFIED_ERROR =
+  "Ada yang salah ni kawan, coba kontak customer service ya biar kamu dibantuin !";
+const STORE_ALREADY_EXIST =
+  "Toko sudah ada, coba pakai nama lain untuk toko baru anda !";
 
 // INTERNAL ERRORS
-const CANT_VALIDATE_RECOVERY_TOKEN = "Recovery token tidak dapat tervalidasi, token mungkin sudah pernah digunakan. \n\n Silahkan request email recovery password lagi ya.";
-const INTERNAL_ERROR_CANT_COMMUNICATE = "INTERNAL ERROR: Can't communicate with the other services.";
+const CANT_VALIDATE_RECOVERY_TOKEN =
+  "Recovery token tidak dapat tervalidasi, token mungkin sudah pernah digunakan. \n\n Silahkan request email recovery password lagi ya.";
+const INTERNAL_ERROR_CANT_COMMUNICATE =
+  "INTERNAL ERROR: Can't communicate with the other services.";
 const KEY_HAS_TO_BE_UNIQUE = "KEY_HAS_TO_BE_UNIQUE";
 
 // CREDENTIALS ERRORS
 const PLEASE_VERIFY_OTP = "PLEASE_VERIFY_OTP";
 const USER_UNAUTHORIZED = "User unauthorized";
-const SESSION_TOKEN_NOT_FOUND = "Session token tidak dapat ditemukan !";
+const SESSION_TOKEN_NOT_FOUND =
+  "Session token tidak dapat ditemukan !";
 
 module.exports = {
-    STORE_ALREADY_EXIST,
-    CANT_VALIDATE_RECOVERY_TOKEN,
-    INTERNAL_ERROR_CANT_COMMUNICATE,
-    KEY_HAS_TO_BE_UNIQUE,
-    UNIDENTIFIED_ERROR,
-    USER_UNAUTHORIZED,
-    PLEASE_VERIFY_OTP,
-    SESSION_TOKEN_NOT_FOUND,
-}
+  STORE_ALREADY_EXIST,
+  CANT_VALIDATE_RECOVERY_TOKEN,
+  INTERNAL_ERROR_CANT_COMMUNICATE,
+  KEY_HAS_TO_BE_UNIQUE,
+  UNIDENTIFIED_ERROR,
+  USER_UNAUTHORIZED,
+  PLEASE_VERIFY_OTP,
+  SESSION_TOKEN_NOT_FOUND,
+};
